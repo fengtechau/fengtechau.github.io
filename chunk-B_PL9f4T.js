6379d9c1dc845ddeb407e008d7d715d26b170e24
fentechau <96329838+fengtechau@users.noreply.github.com>
2026-09-05 16:43:17 +1000

    Auto-generated commit

---
+++ chunk-B_PL9f4T.js
@@ -0,0 +1,1 @@
+import{Ht as v,Kt as x,g as JO}from"./main-EBE655IC.js";var n=`FENG TECH`;var m=(()=>{class t{constructor(){this.title=v(JO)}setTitle(e){this.title.setTitle(`${e} \xB7 ${n}`)}static{this.ɵfac=function(c){return new(c||t)}}static{this.ɵprov=x({token:t,factory:t.ɵfac,providedIn:`root`})}}return t})();export{m as t};
